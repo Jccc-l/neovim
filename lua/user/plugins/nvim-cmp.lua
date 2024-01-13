0bd34c134d1a20a926f88884415418a2371f1751
Jccc-l <1216550215Jc@gmail.com>
2024-01-13 17:19:26 +0800

    Configure LuaSnip

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -3,16 +3,36 @@ return {
 	dependencies = {
 		{
 			"hrsh7th/cmp-nvim-lsp",
+		},
+		{
+			{
+				"L3MON4D3/LuaSnip",
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+				-- install jsregexp (optional!).
+				build = "make install_jsregexp",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
 		}
 	},
 	config = function()
 		local cmp = require 'cmp'
+		local luasnip = require("luasnip")
+		local has_words_before = function()
+			unpack = unpack or table.unpack
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		cmp.setup({
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 				end,
@@ -24,14 +44,33 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
 				['<C-e>'] = cmp.mapping.abort(),
 				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				['<C-j>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						return cmp.select_next_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				['<C-k>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						return cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-				{ name = 'vsnip' }, -- For vsnip users.
-				-- { name = 'luasnip' }, -- For luasnip users.
+				-- { name = 'vsnip' }, -- For vsnip users.
+				{ name = 'luasnip' }, -- For luasnip users.
 				-- { name = 'ultisnips' }, -- For ultisnips users.
 				-- { name = 'snippy' }, -- For snippy users.
 			}, {
