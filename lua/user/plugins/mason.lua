f3b101c489887fe30e1c634ecd47f89c5ae58fc7
Jccc-l <1216550215Jc@gmail.com>
2024-03-15 10:59:15 +0800

    Substitute python-lsp-server for pyright

--- lua/user/plugins/mason.lua
+++ lua/user/plugins/mason.lua
@@ -148,7 +148,7 @@ return {
 		})
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-		require('lspconfig')['pyright'].setup {
+		require('lspconfig')['pylsp'].setup {
 			capabilities = capabilities
 		}
 		require('lspconfig')['clangd'].setup {
