bdcec0b45ef109f9153b5f971805e67b1202347b
Jccc <1216550215Jc@gmail.com>
2024-05-03 11:09:32 +0800

    Remove plugin telescope-ctags-outline.nvim

--- lua/user/plugins/nvim-telescope.lua
+++ lua/user/plugins/nvim-telescope.lua
@@ -2,24 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		{
-			"fcying/telescope-ctags-outline.nvim"
-		}
 	},
 	config = function()
-		require('telescope').setup {
-			extensions = {
-				ctags_outline = {
-					--ctags option
-					ctags = { 'ctags' },
-					--ctags filetype option
-					ft_opt = {
-						vim = '--vim-kinds=fk',
-						lua = '--lua-kinds=fk',
-					},
-				},
-			},
-		}
-		require('telescope').load_extension('ctags_outline')
 	end
 }
